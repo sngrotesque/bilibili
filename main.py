68cb8094af9cac3ac4661223ac4ae5fae9103dc1
SN-Grotesque <z7z-h@outlook.com>
2022-09-03 22:53:07 +0800

    bilibili

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
 import BLBL
 
 
-import requests
-
-url = 'http://cn.bing.com/'
-proxy = {"http":"http://20.24.22.143:8000","https":"http://20.24.22.143:8000"}
-headers = {"User-Agent":"Mozilla/5.0 (X11; Linux i686; rv:97.0) Gecko/20100101 Firefox/97.0"}
-
-res = requests.get(url, headers = headers, proxies = proxy)
-
-print(res.text)
-
